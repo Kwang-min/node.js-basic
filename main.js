f6eaa3febe80549a7c7869627b60cfdfe4a86b9a
Kwang-min <48762168+Kwang-min@users.noreply.github.com>
2020-06-24 17:29:41 +0900

    arrange code using function
    함수를 이용해서 중복되는 코드 정리

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+function templateHTML(title,list,body) {
+  return `
+  <!doctype html>
+<html>
+<head>
+<title>WEB1 - ${title}</title>
+<meta charset="utf-8">
+</head>
+<body>
+<h1><a href="/">WEB</a></h1>
+${list}
+${body}
+</body>
+</html>             
+  `;
+}
+function templateList(filelist) {
+  var list = '<ul>';
+  var i = 0;
+  while (i < filelist.length) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+
+  list = list + '</ul>';
+
+  return list;
+        
+}
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
@@ -17,30 +46,9 @@ var app = http.createServer(function (request, response) {
         var title = 'welcome';
         var description = 'hello, node.js';
 
-        var list = '<ul>';
-        var i = 0;
-        while (i < filelist.length) {
-          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-          i = i + 1;
-        }
+        var list = templateList(filelist);
 
-        list = list + '</ul>';
-
-        var template = `
-        <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      <h2>${title}</h2>
-      <p>${description}</p>
-    </body>
-    </html>             
-        `;
+        var template = templateHTML(title,list,`<h2>${title}</h2>${description}`);
         response.writeHead(200);
         response.end(template);
       });
@@ -50,35 +58,12 @@ var app = http.createServer(function (request, response) {
     } else {
       fs.readdir('./data', function (error, filelist) {
 
-        var title = 'welcome';
-        var description = 'hello, node.js';
-
-        var list = '<ul>';
-        var i = 0;
-        while (i < filelist.length) {
-          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-          i = i + 1;
-        }
-
-        list = list + '</ul>';
+        
 
         fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
           var title = queryData.id;
-          var template = `
-        <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      <h2>${title}</h2>
-      <p>${description}</p>
-    </body>
-    </html>             
-        `;
+          var list = templateList(filelist);
+          var template = templateHTML(title,list,`<h2>${title}</h2>${description}`);
           response.writeHead(200);
           response.end(template);
         });
