49a3d1a1fcd070963b240e068143caf26df83599
Kwang-min <48762168+Kwang-min@users.noreply.github.com>
2020-07-09 19:08:34 +0900

    how to get post data
    post 방식으로 데이터 전송하고 받기

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-function templateHTML(title,list,body) {
+var qs = require('querystring');
+
+const { format } = require('path');
+function templateHTML(title, list, body) {
   return `
   <!doctype html>
 <html>
 <head>
-<title>WEB1 - ${title}</title>
+<title>WEB - ${title}</title>
 <meta charset="utf-8">
 </head>
 <body>
 <h1><a href="/">WEB</a></h1>
 ${list}
+<a href="/create">create</a>
 ${body}
 </body>
 </html>             
@@ -28,7 +32,7 @@ function templateList(filelist) {
   list = list + '</ul>';
 
   return list;
-        
+
 }
 
 var app = http.createServer(function (request, response) {
@@ -38,7 +42,6 @@ var app = http.createServer(function (request, response) {
 
 
   if (pathname === '/') {
-
     if (queryData.id === undefined) {
 
       fs.readdir('./data', function (error, filelist) {
@@ -48,7 +51,7 @@ var app = http.createServer(function (request, response) {
 
         var list = templateList(filelist);
 
-        var template = templateHTML(title,list,`<h2>${title}</h2>${description}`);
+        var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
         response.writeHead(200);
         response.end(template);
       });
@@ -58,12 +61,12 @@ var app = http.createServer(function (request, response) {
     } else {
       fs.readdir('./data', function (error, filelist) {
 
-        
+
 
         fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
           var title = queryData.id;
           var list = templateList(filelist);
-          var template = templateHTML(title,list,`<h2>${title}</h2>${description}`);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
           response.writeHead(200);
           response.end(template);
         });
@@ -71,7 +74,41 @@ var app = http.createServer(function (request, response) {
 
     }
 
-  } else {
+  } else if (pathname === '/create') {
+    fs.readdir('./data', function (error, filelist) {
+
+      var title = 'WEB-CREATE';
+
+      var list = templateList(filelist);
+
+      var template = templateHTML(title, list, `
+      <form action="http://localhost:3000/create_process" method="post">
+      <p><input type="text" name="title" placeholder="title"></p>
+      <p>
+          <textarea name="description" placeholder="description"></textarea>
+      </p>
+      <p>
+          <input type="submit">
+      </p>
+      </form>
+      `);
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else if (pathname === '/create_process') {
+    var body = '';
+    request.on('data',function(data) {
+      body = body + data;
+    });
+    request.on('end', function() {
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+    })
+    response.writeHead(200);
+    response.end('seccuess');
+  }
+  else {
     response.writeHead(404);
     response.end('Not found');
   }
