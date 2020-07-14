79fb49341657981b8c7ece8415fd2135d23dd5e9
Kwang-min <48762168+Kwang-min@users.noreply.github.com>
2020-07-14 19:17:44 +0900

    refactorying
    모듈화하기

--- main.js
+++ main.js
@@ -2,38 +2,9 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
+var template = require("./lib/template.js");
 
-const { format } = require('path');
-function templateHTML(title, list, body, control) {
-  return `
-  <!doctype html>
-<html>
-<head>
-<title>WEB - ${title}</title>
-<meta charset="utf-8">
-</head>
-<body>
-<h1><a href="/">WEB</a></h1>
-${list}
-${control}
-${body}
-</body>
-</html>             
-  `;
-}
-function templateList(filelist) {
-  var list = '<ul>';
-  var i = 0;
-  while (i < filelist.length) {
-    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-    i = i + 1;
-  }
 
-  list = list + '</ul>';
-
-  return list;
-
-}
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
@@ -49,13 +20,13 @@ var app = http.createServer(function (request, response) {
         var title = 'welcome';
         var description = 'hello, node.js';
 
-        var list = templateList(filelist);
+        var list = template.list(filelist);
 
-        var template = templateHTML(title, list,
+        var html = template.HTML(title, list,
           `<h2>${title}</h2>${description}`,
           `<a href="/create">create</a>`);
         response.writeHead(200);
-        response.end(template);
+        response.end(html);
       });
 
 
@@ -67,8 +38,8 @@ var app = http.createServer(function (request, response) {
 
         fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
           var title = queryData.id;
-          var list = templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`,
+          var list = template.list(filelist);
+          var html = template.HTML(title, list, `<h2>${title}</h2>${description}`,
             `<a href="/create">create</a> 
             <a href="/update?id=${title}">update</a>
             <form action="delete_process" method="post">
@@ -78,7 +49,7 @@ var app = http.createServer(function (request, response) {
             `
           );
           response.writeHead(200);
-          response.end(template);
+          response.end(html);
         });
       });
 
@@ -89,9 +60,9 @@ var app = http.createServer(function (request, response) {
 
       var title = 'WEB-CREATE';
 
-      var list = templateList(filelist);
+      var list = template.list(filelist);
 
-      var template = templateHTML(title, list, `
+      var html = template.HTML(title, list, `
       <form action="/create_process" method="post">
       <p><input type="text" name="title" placeholder="title"></p>
       <p>
@@ -103,7 +74,7 @@ var app = http.createServer(function (request, response) {
       </form>
       `, '');
       response.writeHead(200);
-      response.end(template);
+      response.end(html);
     });
   } else if (pathname === '/create_process') {
     var body = '';
@@ -125,8 +96,8 @@ var app = http.createServer(function (request, response) {
     fs.readdir('./data', function (error, filelist) {
       fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
         var title = queryData.id;
-        var list = templateList(filelist);
-        var template = templateHTML(title, list, `
+        var list = template.list(filelist);
+        var html = template.HTML(title, list, `
         <form action="/update_process" method="post">
         <input type="hidden" name="id" value = "${title}">
       <p><input type="text" name="title" placeholder="title" value="${title}"></p>
@@ -141,10 +112,10 @@ var app = http.createServer(function (request, response) {
           `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
         );
         response.writeHead(200);
-        response.end(template);
+        response.end(html);
       });
     });
-
+ 
   } else if (pathname === "/update_process") {
     var body = '';
     request.on('data', function (data) {
@@ -155,7 +126,7 @@ var app = http.createServer(function (request, response) {
       var id = post.id;
       var title = post.title;
       var description = post.description;
-      fs.rename(`data/${id}`, `data/${title}`, function(error){
+      fs.rename(`data/${id}`, `data/${title}`, function (error) {
         fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
           response.writeHead(302, { Location: `/?id=${title}` });
           response.end();
@@ -170,10 +141,10 @@ var app = http.createServer(function (request, response) {
     request.on('end', function () {
       var post = qs.parse(body);
       var id = post.id;
-      fs.unlink(`data/${id}`, function(error) {
+      fs.unlink(`data/${id}`, function (error) {
         response.writeHead(302, { Location: `/` });
         response.end();
-          
+
       });
     })
   }
